110898b1305b15d7c9432e3e327098a89c8ec80d
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-07-31 00:14:19 +0200

    Stop splitToCleanRows from erroring if end marker is not found
    Happens too often when streaming

--- packages/core/src/parse.ts
+++ packages/core/src/parse.ts
@@ -6,9 +6,12 @@ export function splitToCleanRows(payload: string) {
   const rows = payload.split("\n");
 
   if (rows.at(-1) !== "") {
-    throw new Error(
-      "RSC payload is missing an empty newline at the end indicating that it is not complete.",
+    console.error(
+      "RSC payload is missing an empty newline at the end indicating that it is not complete."
     );
+    // throw new Error(
+    //   "RSC payload is missing an empty newline at the end indicating that it is not complete.",
+    // );
   }
 
   rows.pop();
@@ -87,11 +90,11 @@ export function parse(tokens: ReturnType<typeof lexer>) {
       (token) =>
         token.type === "DOUBLE_QUOTE" ||
         token.type === "LEFT_BRACE" ||
-        token.type === "LEFT_BRACKET",
+        token.type === "LEFT_BRACKET"
     );
     const tokensBetweenColonAndJson = tokens.slice(
       firstColonIndex + 1,
-      firstJsonStartIndex,
+      firstJsonStartIndex
     );
     const type = tokensBetweenColonAndJson.map((token) => token.value).join("");
 
@@ -103,7 +106,7 @@ export function parse(tokens: ReturnType<typeof lexer>) {
       (token) =>
         token.type === "DOUBLE_QUOTE" ||
         token.type === "LEFT_BRACE" ||
-        token.type === "LEFT_BRACKET",
+        token.type === "LEFT_BRACKET"
     );
     const tokensAfterJsonStart = tokens.slice(firstJsonStartIndex);
     const data = tokensAfterJsonStart.map((token) => token.value).join("");
