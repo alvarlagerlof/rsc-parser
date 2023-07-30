c70d81691a916127bc9974ee7f88ec1f1d517aeb
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-07-30 20:39:47 +0200

    refactor(app): move parser to core
    To be used by other things later

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -2,5 +2,15 @@ import { Meter } from "./Meter";
 import { ClientReferenceRow } from "./rows/ClientReferenceRow";
 import { HintRow } from "./rows/HintRow";
 import { TreeRow } from "./rows/TreeRow";
+import { parse, refineRowType, lexer, splitToCleanRows } from "./parse";
 
-export { TreeRow, ClientReferenceRow, HintRow, Meter };
+export {
+  TreeRow,
+  ClientReferenceRow,
+  HintRow,
+  Meter,
+  parse,
+  refineRowType,
+  lexer,
+  splitToCleanRows,
+};
