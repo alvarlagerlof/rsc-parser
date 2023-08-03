2881308cd23e6bc907519dc5eb09f3f829252cba
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-08-03 17:08:52 +0200

    Fix scroll overflow in Layout.tsx (#159)

--- packages/chrome-extension/src/Layout.tsx
+++ packages/chrome-extension/src/Layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-full space-y-8 bg-slate-100 p-6 dark:bg-slate-900">
+    <div className="h-full space-y-8 overflow-y-scroll bg-slate-100 p-6 dark:bg-slate-900">
       <h1 className="text-3xl dark:text-white">RSC Devtools</h1>
 
       {children}
