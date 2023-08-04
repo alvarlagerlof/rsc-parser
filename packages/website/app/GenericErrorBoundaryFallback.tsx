316d8b18fc53074f99f61fcacd9d1e85496b3128
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-08-04 22:02:08 +0200

    Remove unused error boundary fallback component (#171)
    Was used before, but is not imported anywhere now.

--- packages/website/app/GenericErrorBoundaryFallback.tsx
+++
@@ -1,8 +0,0 @@
-export function GenericErrorBoundaryFallback({ error }: { error: Error }) {
-  return (
-    <div role="alert" className="rounded-lg bg-red-100 p-4">
-      <p>Something went wrong:</p>
-      <pre className="text-red-600">{error.message}</pre>
-    </div>
-  );
-}
