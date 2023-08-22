3b623df9236ecf9f5ae83b36d419f4a4bec98353
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-08-22 23:37:33 +0200

    Improve extension error boundary in dark mode (#260)

--- packages/core/src/GenericErrorBoundaryFallback.tsx
+++ packages/core/src/GenericErrorBoundaryFallback.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export function GenericErrorBoundaryFallback({ error }: { error: Error }) {
   return (
     <div role="alert" className="rounded-lg bg-red-100 p-4 dark:text-white">
-      <p>Something went wrong:</p>
-      <pre className="text-red-600">{error.message}</pre>
+      <p className="dark:text-white">Something went wrong:</p>
+      <pre className="text-red-600 dark:text-red-400">{error.message}</pre>
     </div>
   );
 }
