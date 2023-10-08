5ed87525b46dbd25fb7a0b7542e5dc6e9869e5b8
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-10-08 14:40:15 +0200

    Continue setting up changeset (#401)
    - Created a version script
    - Linked package versions

---
+++ .changeset/heavy-stingrays-poke.md
@@ -0,0 +1,7 @@
+---
+"@rsc-parser/chrome-extension": patch
+"@rsc-parser/website": patch
+"@rsc-parser/core": patch
+---
+
+Continued setting up changesets
---
+++ apply-version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Step 1: Get the changeset status
+yarn changeset status --output version.json
+
+# Step 2: Extract newVersion from version.json
+new_version=$(jq -r '.releases[0].newVersion' version.json)
+echo "Found new version: $new_version"
+
+# Step 3: Update the version in manifest.json
+jq --arg version "$new_version" '.version = $version' packages/chrome-extension/public/manifest.json > tmp.json && mv tmp.json packages/chrome-extension/public/manifest.json
+echo "Applied new version: $_new_version"
+
+# Step 4: Clean up
+rm version.json
