a20022600be19242fc05c44d7f26c4b969b124e7
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2024-01-02 19:47:36 +0100

    network graph!

--- packages/core/src/viewer/StreamViewer.tsx
+++ packages/core/src/viewer/StreamViewer.tsx
@@ -12,14 +12,18 @@ import * as Ariakit from "@ariakit/react";
 import { RawStream } from "../stream/RawStream";
 import { RowStream } from "../stream/RowStream";
 import {
+  Chunk,
   FlightResponse,
   createFromJSONCallback,
+  isParsedObject,
   processBinaryChunk,
 } from "../react/ReactFlightClient";
 import { createStringDecoder } from "../react/ReactFlightClientConfigBrowser";
+import { useEffect, useRef } from "react";
+import * as d3 from "d3";
 
 export function StreamViewer({ messages }: { messages: RscChunkMessage[] }) {
-  const defaultSelectedId = "parsed";
+  const defaultSelectedId = "tree";
   const tab = Ariakit.useTabStore({ defaultSelectedId });
 
   const timeScrubber = useTimeScrubber(messages, {
@@ -67,12 +71,12 @@ export function StreamViewer({ messages }: { messages: RscChunkMessage[] }) {
                 className="flex flex-row gap-2"
               >
                 <Ariakit.Tab
-                  id="parsed"
+                  id="tree"
                   className="rounded-md px-2 py-0.5 aria-selected:bg-slate-300 dark:text-white dark:aria-selected:text-black"
                 >
-                  Debug
+                  Tree
                 </Ariakit.Tab>
-                {/* <Ariakit.Tab
+                <Ariakit.Tab
                   id="parsed"
                   className="rounded-md px-2 py-0.5 aria-selected:bg-slate-300 dark:text-white dark:aria-selected:text-black"
                 >
@@ -89,12 +93,19 @@ export function StreamViewer({ messages }: { messages: RscChunkMessage[] }) {
                   className="rounded-md px-2 py-0.5 aria-selected:bg-slate-300 dark:text-white dark:aria-selected:text-black"
                 >
                   Raw
-                </Ariakit.Tab> */}
+                </Ariakit.Tab>
               </Ariakit.TabList>
             </div>
             <div>
               <Ariakit.TabPanel store={tab} tabId={defaultSelectedId}>
+                <DebugTree messages={messagesForCurrentTab} />
+              </Ariakit.TabPanel>
+              <Ariakit.TabPanel store={tab}>
                 <DebugRaw messages={messagesForCurrentTab} />
+              </Ariakit.TabPanel>
+
+              <Ariakit.TabPanel store={tab}>
+                <RowStream messages={messagesForCurrentTab} />
               </Ariakit.TabPanel>
               {/* <Ariakit.TabPanel store={tab} tabId={defaultSelectedId}>
                 <RowTabs
@@ -117,7 +128,7 @@ export function StreamViewer({ messages }: { messages: RscChunkMessage[] }) {
   );
 }
 
-function DebugRaw({ messages }: { messages: RscChunkMessage[] }) {
+function messagesToResponse(messages: RscChunkMessage[]) {
   if (messages.length === 0) {
     return null;
   }
@@ -149,7 +160,7 @@ function DebugRaw({ messages }: { messages: RscChunkMessage[] }) {
     _rowLength: 0,
     _rowState: 0,
     _stringDecoder: createStringDecoder(),
-    _chunks: [],
+    _chunks: [] as FlightResponse["_chunks"],
   } satisfies FlightResponse;
 
   response._fromJSON = createFromJSONCallback(response);
@@ -170,6 +181,12 @@ function DebugRaw({ messages }: { messages: RscChunkMessage[] }) {
   //   processBinaryChunk(response, responseBuffer[i]);
   // }
 
+  return response;
+}
+
+function DebugRaw({ messages }: { messages: RscChunkMessage[] }) {
+  const response = messagesToResponse(messages);
+
   return (
     <>
       <RowTabs chunks={response._chunks} />
@@ -187,6 +204,202 @@ function DebugRaw({ messages }: { messages: RscChunkMessage[] }) {
 
       <p>Raw:</p>
       <pre>{JSON.stringify(messages, null, 2)}</pre> */}
+    </>
+  );
+}
+
+interface Node extends d3.SimulationNodeDatum {
+  id: string;
+}
+
+interface Link extends d3.SimulationLinkDatum<Node> {
+  source: string;
+  target: string;
+}
+
+export type Data = {
+  nodes: Node[];
+  links: Link[];
+};
+
+export const RADIUS = 10;
+
+export const drawNetwork = (
+  context: CanvasRenderingContext2D,
+  width: number,
+  height: number,
+  nodes: Node[],
+  links: Link[],
+) => {
+  context.clearRect(0, 0, width, height);
+
+  // Draw the links first
+  links.forEach((link) => {
+    context.beginPath();
+    context.moveTo(link.source.x, link.source.y);
+    context.lineTo(link.target.x, link.target.y);
+    context.strokeStyle = "#ffffff";
+    context.stroke();
+  });
+
+  // Draw the nodes
+  nodes.forEach((node) => {
+    if (!node.x || !node.y) {
+      return;
+    }
+
+    context.beginPath();
+    context.moveTo(node.x + RADIUS, node.y);
+    context.arc(node.x, node.y, RADIUS, 0, 2 * Math.PI);
+    context.fillStyle = "#cb1dd1";
+    context.fill();
+  });
+};
+
+export const NetworkDiagram = ({
+  width,
+  height,
+  data,
+}: {
+  width: number;
+  height: number;
+  data: Data;
+}) => {
+  // The force simulation mutates links and nodes, so create a copy first
+  // Node positions are initialized by d3
+  const links: Link[] = data.links.map((d) => ({ ...d }));
+  const nodes: Node[] = data.nodes.map((d) => ({ ...d }));
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    // set dimension of the canvas element
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext("2d");
+
+    if (!context) {
+      return;
+    }
+
+    // run d3-force to find the position of nodes on the canvas
+    d3.forceSimulation(nodes)
+
+      // list of forces we apply to get node positions
+      .force(
+        "link",
+        d3.forceLink<Node, Link>(links).id((d) => d.id),
+      )
+      .force("collide", d3.forceCollide().radius(RADIUS))
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2))
+
+      // at each iteration of the simulation, draw the network diagram with the new node positions
+      .on("tick", () => {
+        drawNetwork(context, width, height, nodes, links);
+      });
+  }, [width, height, nodes, links]);
+
+  return (
+    <div>
+      <canvas
+        ref={canvasRef}
+        style={{
+          width,
+          height,
+        }}
+        width={width}
+        height={height}
+      />
+    </div>
+  );
+};
+
+function findReferencesInChunk(chunk: Chunk) {
+  if (chunk.type !== "model") {
+    return [];
+  }
+
+  const references: string[] = [];
+
+  function walk(data: unknown) {
+    if (isParsedObject(data)) {
+      references.push(data.id);
+      return;
+    }
+
+    if (Array.isArray(data)) {
+      for (const value of data) {
+        walk(value);
+      }
+      return;
+    }
+
+    if (typeof data == "object" && data !== null) {
+      for (const value of Object.values(data)) {
+        walk(value);
+      }
+    }
+  }
+
+  walk(chunk.value);
+
+  return references;
+}
+
+function getChunkById(chunks: Chunk[], id: string) {
+  return chunks.find((chunk) => chunk.id === id);
+}
+
+function getLinks(chunks: Chunk[], id: string) {
+  const links: Link[] = [];
+
+  function walk(id: string) {
+    const chunk = getChunkById(chunks, id);
+    if (!chunk) {
+      return links;
+    }
+
+    const references = findReferencesInChunk(chunk);
+    for (const reference of references) {
+      links.push({
+        source: id,
+        target: reference,
+      });
+
+      walk(reference);
+    }
+  }
+
+  walk(id);
+
+  return links;
+}
+
+function DebugTree({ messages }: { messages: RscChunkMessage[] }) {
+  const response = messagesToResponse(messages);
+
+  if (!response) {
+    return null;
+  }
+
+  const nodes = response._chunks.map((chunk) => {
+    return {
+      id: chunk.id,
+    };
+  });
+
+  const links = getLinks(response._chunks, "0");
+
+  const data = {
+    nodes,
+    links,
+  } satisfies Data;
+
+  return (
+    <>
+      <pre>{JSON.stringify(nodes, null, 2)}</pre>
+      <pre>{JSON.stringify(links, null, 2)}</pre>
+      <NetworkDiagram width={500} height={500} data={data} />
     </>
   );
 }
