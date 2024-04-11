572e2fcda3affa7ccaf1d7939588da6800d40d56
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2024-04-11 22:29:08 +0200

    Improve DebugCopyMessagesButton story with real data

--- packages/core/src/components/DebugCopyMessagesButton.stories.tsx
+++ packages/core/src/components/DebugCopyMessagesButton.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { DebugCopyMessagesButton } from "./DebugCopyMessagesButton";
+import { alvarDevExampleData } from "../example-data/alvar-dev";
 
 const meta: Meta<typeof DebugCopyMessagesButton> = {
   component: DebugCopyMessagesButton,
@@ -11,4 +12,7 @@ type Story = StoryObj<typeof DebugCopyMessagesButton>;
 
 export const example: Story = {
   name: "example",
+  args: {
+    messages: alvarDevExampleData,
+  },
 };
