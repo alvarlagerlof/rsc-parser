276d9e5d63215cb05e9653625f1acaf5c1061ed6
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2024-05-10 19:46:11 +0200

    Add `typecheck` command

--- packages/chrome-extension/src/react-preamble.ts
+++ packages/chrome-extension/src/react-preamble.ts
@@ -1,5 +1,9 @@
+// @ts-expect-error Vite special sauce
 import RefreshRuntime from "http://localhost:6020/@react-refresh";
 RefreshRuntime.injectIntoGlobalHook(window);
+// @ts-expect-error Vite special sauce
 window.$RefreshReg$ = () => {};
+// @ts-expect-error Vite special sauce
 window.$RefreshSig$ = () => (type) => type;
+// @ts-expect-error Vite special sauce
 window.__vite_plugin_react_preamble_installed__ = true;
