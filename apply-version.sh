f4dd7aff5a98128a7a97186365a94c839e93fd10
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2024-06-09 00:26:03 +0200

    Fix versioning script (#1040)
    It seems that `yarn changeset status --since origin/main --output version.json` no longer works as before (see https://github.com/changesets/changesets/issues/700)
    
    It's needed to update the manifest file for the chrome extension. Instead we can apply the version to all package.json files and read it from one of them.

--- apply-version.sh
+++ apply-version.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -e
 
-# Step 1: Get the changeset status
-yarn changeset status --since origin/main --output version.json
-
-# Step 2: Extract newVersion from version.json
-new_version=$(jq -r '.releases[0].newVersion' version.json)
+# Step 1: Extract newVersion from ./packages/core/package.json
+new_version=$(jq -r '.version' ./packages/core/package.json)
 echo "Found new version: $new_version"
 
-# Step 3: Update the version in manifest.json
+# Step 2: Update the version in manifest.json
 jq --arg version "$new_version" '.version = $version' packages/chrome-extension/public/manifest.json > tmp.json && mv tmp.json packages/chrome-extension/public/manifest.json
 echo "Applied new version: $_new_version"
 
-# Step 4: Clean up
+# Step 3: Clean up
 yarn prettier -w packages/chrome-extension/public/manifest.json
-rm version.json
