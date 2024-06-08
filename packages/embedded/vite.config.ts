095e40e64745dde0cb2882ebcb78b32cb3b042c1
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2024-06-09 00:46:21 +0200

    Ensure that `react` and `react-dom` are externalized (#1041)
    * Ensure that `react` and `react-dom` are externalized
    
    Had some trouble at work with `@rsc-parser/embedded` that may have come from there potentially being two copies `react-dom`.
    
    * Run yarn install

--- packages/embedded/vite.config.ts
+++ packages/embedded/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["react"],
+      external: ["react", "react-dom"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
