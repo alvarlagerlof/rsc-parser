44a478eace53476668a26e7cfa3ff87f4730b873
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2024-06-10 01:36:03 +0200

    Create a Timings tab (#1063)
    A new way to see the time network time between chunks.

---
+++ packages/core/src/components/RequestDetailTabTimings.stories.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { RequestDetailTabTimings } from "./RequestDetailTabTimings";
+import { alvarDevExampleData } from "../example-data/alvar-dev";
+import { ghFredkissDevExampleData } from "../example-data/gh-fredkiss-dev";
+import { nextjsOrgExampleData } from "../example-data/nextjs-org";
+
+const meta: Meta<typeof RequestDetailTabTimings> = {
+  component: RequestDetailTabTimings,
+};
+
+export default meta;
+type Story = StoryObj<typeof RequestDetailTabTimings>;
+
+export const alvarDev: Story = {
+  name: "alvar.dev",
+  render: () => {
+    return <RequestDetailTabTimings events={alvarDevExampleData} />;
+  },
+};
+
+export const ghFredKissDev: Story = {
+  name: "gh.fredkiss.dev",
+  render: () => {
+    return <RequestDetailTabTimings events={ghFredkissDevExampleData} />;
+  },
+};
+
+export const nextjsOrg: Story = {
+  name: "nextjs.org",
+  render: () => {
+    return <RequestDetailTabTimings events={nextjsOrgExampleData} />;
+  },
+};
---
+++ packages/core/src/components/RequestDetailTabTimings.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { eventsFilterByMaxTimestamp } from "../eventArrayHelpers";
+import {
+  RscEvent,
+  isRscChunkEvent,
+  isRscRequestEvent,
+  isRscResponseEvent,
+} from "../events";
+import { useEndTime } from "./EndTimeContext";
+import { RequestDetailTabEmptyState } from "./RequestDetailTabEmptyState";
+import { getColorForFetch } from "../color";
+
+export function RequestDetailTabTimings({ events }: { events: RscEvent[] }) {
+  const { endTime } = useEndTime();
+
+  const timeFilteredEvents = eventsFilterByMaxTimestamp(events, endTime);
+
+  if (timeFilteredEvents.length === 0) {
+    return <RequestDetailTabEmptyState />;
+  }
+
+  const requestEvent = timeFilteredEvents.filter(isRscRequestEvent)[0];
+  const responseEvent = timeFilteredEvents.filter(isRscResponseEvent)[0];
+
+  if (!requestEvent) {
+    return <RequestDetailTabEmptyState />;
+  }
+
+  const startTimeStamp = requestEvent.data.timestamp;
+  const endTimestamp = events.at(-1)?.data.timestamp;
+
+  if (!endTimestamp) {
+    return <RequestDetailTabEmptyState />;
+  }
+
+  const timings: {
+    name: string;
+    offset: number | null;
+    color: string;
+  }[] = [];
+
+  timings.push({
+    name: "Request start",
+    offset: startTimeStamp - startTimeStamp,
+    color: getColorForFetch("1"),
+  });
+
+  if (responseEvent) {
+    timings.push({
+      name: "Response start",
+      offset: responseEvent.data.timestamp - startTimeStamp,
+      color: getColorForFetch("20"),
+    });
+
+    timeFilteredEvents.filter(isRscChunkEvent).forEach((event, index) => {
+      timings.push({
+        name: `Chunk ${index}`,
+        offset: event.data.timestamp - startTimeStamp,
+        color: getColorForFetch("30"),
+      });
+    });
+
+    if (
+      timeFilteredEvents.find((event) => event.data.timestamp === endTimestamp)
+    ) {
+      timings.push({
+        name: "Response end",
+        offset: endTimestamp ? endTimestamp - startTimeStamp : null,
+        color: getColorForFetch("20"),
+      });
+    }
+  }
+
+  const maxOffset = (endTimestamp ?? 500) - startTimeStamp;
+
+  return (
+    <ul className="flex max-w-lg flex-col gap-3">
+      {timings.map((timing) => {
+        return (
+          <li key={timing.name} className="flex flex-col gap-0">
+            <p>{timing.name}</p>
+            <div className="flex flex-row items-center gap-2">
+              <div
+                className="h-3 rounded-full"
+                style={{
+                  background: timing.color,
+                  width: getWidthByOffset(timing.offset, maxOffset),
+                }}
+              />
+              {timing.offset !== null ? <p>{timing.offset}ms</p> : <p>-</p>}
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function getWidthByOffset(offset: number | null, biggestOffset: number) {
+  if (offset === null) {
+    return "100%";
+  }
+
+  if (offset === 0) {
+    return "3px";
+  }
+
+  return `${(offset / biggestOffset) * 100}%`;
+}
