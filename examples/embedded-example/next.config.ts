d1f746dfd0ad4c81364bab4c8596850140c57afd
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2024-12-07 17:22:03 +0100

    Upgrade to Next.js 15

---
+++ examples/embedded-example/next.config.ts
@@ -0,0 +1,5 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {};
+
+export default nextConfig;
--- packages/website/next.config.ts
+++ packages/website/next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
@@ -14,4 +15,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
