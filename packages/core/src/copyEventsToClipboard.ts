1866207d85bc69d2c1363fce9c20313454748bc9
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2025-03-31 22:44:35 +0200

    Remove unused @ts-expect-error comment

--- packages/core/src/copyEventsToClipboard.ts
+++ packages/core/src/copyEventsToClipboard.ts
@@ -4,7 +4,6 @@ export function copyEventsToClipboard({ events }: { events: RscEvent[] }) {
   const stingifiedEvents = JSON.stringify(events);
   console.log(stingifiedEvents);
   const input = document.createElement('input');
-  // @ts-expect-error This is a hack
   input.style = 'position: absolute; left: -1000px; top: -1000px';
   input.value = stingifiedEvents;
   document.body.appendChild(input);
